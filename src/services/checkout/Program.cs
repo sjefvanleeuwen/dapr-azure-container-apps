0ec6bedd6a28768b45add4e2b564daf66a1f08b5
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2022-07-07 22:51:28 +0200

    correcting topic name

--- src/services/checkout/Program.cs
+++ src/services/checkout/Program.cs
@@ -8,7 +8,7 @@ for (int i = 1; i <= 10; i++) {
     using var client = new DaprClientBuilder().Build();
 
     // Publish an event/message using Dapr PubSub
-    await client.PublishEventAsync("pubsub", "new Order", order);
+    await client.PublishEventAsync("pubsub", "newOrder", order);
     Console.WriteLine("Published data: " + order);
 
     await Task.Delay(TimeSpan.FromSeconds(1));
