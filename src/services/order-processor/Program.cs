c7912bc249c9a05695f77a050d3b3bb5bb1a2d6e
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2022-09-04 14:06:10 +0200

    adding state test

--- src/services/order-processor/Program.cs
+++ src/services/order-processor/Program.cs
@@ -1,24 +1,37 @@
 using System.Text.Json.Serialization;
 using Dapr;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDaprClient();
+//builder.Services.AddControllers().AddDapr();
 var app = builder.Build();
-
 // Dapr will send serialized event object vs. being raw CloudEvent
 app.UseCloudEvents();
+var client = app.Services.GetRequiredService<DaprClient>();
 
 // needed for Dapr pub/sub routing
 app.MapSubscribeHandler();
 
-if (app.Environment.IsDevelopment()) {app.UseDeveloperExceptionPage();}
+SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
 
+
+if (app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 // Dapr subscription in [Topic] routes orders topic to this route
-app.MapPost("/orders", [Topic("pubsub", "newOrder")] (Order order) => {
-    Console.WriteLine("New Order Subscriber received : " + order);
+app.MapPost("/orders", [Topic("pubsub", "newOrder")] async (Order order) =>
+{
+    await semaphoreSlim.WaitAsync();
+    var total = await client.GetStateAsync<int>("statestore", "order-total", ConsistencyMode.Eventual);
+    total++;
+    await client.SaveStateAsync<int>("statestore", "order-total", total);
+    total = await client.GetStateAsync<int>("statestore", "order-total", ConsistencyMode.Eventual);
+    Console.WriteLine($"Total Orders {total} New Order Subscriber received : " + order);
+    semaphoreSlim.Release();
     return Results.Ok(order);
-});
 
+});
+//[FromState("statestore", "total-orders")] StateEntry<int> total,
 await app.RunAsync();
 
 public record Order([property: JsonPropertyName("orderId")] int OrderId);
